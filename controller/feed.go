884ecf8ad4840846560714380f4ae8412fe006ef
SIN5t <97775060+SIN5t@users.noreply.github.com>
2023-09-04 15:46:51 +0800

    fix: feed接口单次刷新 attention: 配置内网穿透，需要给MinIO也配置一份。

--- controller/feed.go
+++ controller/feed.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/goForward/tictok_simple_version/domain"
@@ -15,14 +16,11 @@ func Feed(c *gin.Context) {
 	userId := c.GetInt64("userId")
 	//根据接口文档，前端传来的request中有token和latest_time， 这里一个用于存当前用户id，一个存下次视频时间戳
 	//tokenReq := c.Query("token")
-	latestTimeReq := c.Query("latest_time")                         //字符串类型
-	latestTimeInt64, err := strconv.ParseInt(latestTimeReq, 10, 64) //转为时间戳
-	if err != nil {
-		log.Println(err.Error())
-		c.JSON(http.StatusOK, domain.Response{StatusCode: 1, StatusMsg: "时间戳格式错误"}) //定义1为错误的返回
-		return
+	latestTimeReq := c.Query("latest_time") //字符串类型
+	latestTimeInt64 := time.Now().UnixMilli()
+	if latestTimeReq != "" {
+		latestTimeInt64, _ = strconv.ParseInt(latestTimeReq, 10, 64) //转为时间戳
 	}
-
 	videoList, nextTimeInt64, err := service.FeedService(userId, latestTimeInt64)
 	if err != nil {
 		log.Println(err.Error())
